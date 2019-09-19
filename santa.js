33b2cbd7bfa6545a9d1079a77709c9e18e4cbda3
Patrick O'Connor <poc.967@gmail.com>
2019-09-18 21:30:52 -0400

    rewrote function as recursive

--- santa.js
+++ santa.js
@@ -1,5 +1,13 @@
 function deliverPresents(houses) {
-  console.log('Delivering presents to ' + houses[0])
+  if (houses.length === 1) {
+    console.log('Delivering presents to ' + houses[0])
+  } else {
+    var first = houses.slice(0, parseInt((houses.length / 2)))
+    var second = houses.slice(parseInt(houses.length / 2), houses.length)
+
+    deliverPresents(first)
+    deliverPresents(second)
+  }
 }
 
 module.exports = deliverPresents
